9e33b9aefc16e5001b7fdb732b41e67f79e32393
Thomas Moore <moore.thomas9911@gmail.com>
2017-11-25 02:41:31 +1300

    Conversion is hard

--- src/SnakeMachineLearning.java
+++ src/SnakeMachineLearning.java
@@ -1,11 +1,15 @@
+import java.awt.Robot;
+
 public class SnakeMachineLearning {
+	
+	static Robot robot;
 
 	static displayInput snakeGame;
 	
 	public static void main(String[] args) throws Exception { //this is the main function that runs the whole program
 		
 		while(displayInput.test() == true){ //adds an infinite loop
-			
+			//robot.delay(50);
 			displayInput.screenCapture();  //returns a picture of the screen
 			snakeDisplay(); //turns the image into a readable image for the code
 		}
--- src/displayInput.java
+++ src/displayInput.java
@@ -1,22 +1,39 @@
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.List;
 import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 
 import java.io.File;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 
 
 public class displayInput {
 	
+	//Screen Dimensions here:
+	static int xPos = 610; //The starting x and y co-ords for the game
+	static int yPos = 318;
+	
+	static int width = 680; //width and height of the game
+	static int height = 600;
+	
 	//fields
 	static Dimension screenDim;
 	static BufferedImage img;
 	static Robot robot;
 	
+	//color setups
+	static int r;
+	static int g;
+	static int b;
+	static Color col = new Color(r, g, b);
 	
+	
+		
 	public displayInput() {
 		
 	}
@@ -28,8 +45,20 @@ public class displayInput {
 		
 		robot = new Robot();
 		
-		img = robot.createScreenCapture(new Rectangle(610, 318, 680, 600)); //saves the image here
+		img = robot.createScreenCapture(new Rectangle(xPos, yPos, width, height)); //saves the image here
+	
+		for(int i=0;i<=15;i++){
+			for(int j = 0; j<=17; j++){
+				col = robot.getPixelColor(i*40+20,j*40+20);
+				if(b>=140){
+					
+				} else if(r>=140){
+					
+				}
+			}
+		}
 		
+		//This line of code may be useless but was here for debugging purposes
 		ImageIO.write(img, "jpeg",
 				new File("/C:/Users/dickhead moore/Desktop/Uni/my Code Stuff/ScreenCapture/"
 		        + System.currentTimeMillis() + ".jpeg")); //adds the saved image to a folder (for now)
