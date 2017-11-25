3b355a7269573fbab9aad514f89f6cf61a34a87d
Thomas Moore <moore.thomas9911@gmail.com>
2017-11-25 15:34:31 +1300

    Got keys working
    Snake can now respond to commands and read an image as well. Time for the neural network

--- src/Keyboard.java
+++ src/Keyboard.java
@@ -9,31 +9,6 @@ public class Keyboard {
 	static Robot input;
 	
 	//key presses
-	public boolean pressDown() throws AWTException {
-		input = new Robot();
-		input.keyPress(KeyEvent.VK_S);
-		input.keyRelease(KeyEvent.VK_S);
-		return true;
-	}
 	
-	void pressUp() {
-		input.keyPress(38);
-		input.keyRelease(38);
-	}
-	
-	void pressRight() {
-		input.keyPress(KeyEvent.VK_D);
-		input.keyRelease(KeyEvent.VK_D);
-	}
-	
-	void pressLeft() {
-		input.keyPress(KeyEvent.VK_A);
-		input.keyRelease(KeyEvent.VK_A);
-	}
-	
-	void pressSpace() {
-		input.keyPress(KeyEvent.VK_SPACE);
-		input.keyRelease(KeyEvent.VK_SPACE);
-	}
 }
 
--- src/SnakeMachineLearning.java
+++ src/SnakeMachineLearning.java
@@ -1,14 +1,20 @@
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
 
 public class SnakeMachineLearning {
 
 	static displayInput snakeGame;
-	static Keyboard moveSnake;
+	static Robot input;
 	
 	
 	public static void main(String[] args) throws Exception { //this is the main function that runs the whole program
 		
-		Thread.sleep(3000);
-		//snakeMove();
+		input = new Robot(); //setup
+		
+		Thread.sleep(4000); //4 Second wait time
+		
+		pressRight(); //initial start
+		
 		
 		while(displayInput.test() == true){ //adds an infinite loop
 			displayInput.screenCapture();  //returns a picture of the screen
@@ -16,7 +22,36 @@ public class SnakeMachineLearning {
 		}
 	}
 	
-	static void snakeMove() throws Exception { 
-		moveSnake.pressDown();
+	
+	void snakeMove() throws Exception { 
+
+	}
+	
+	
+	
+	//Movements
+	static void pressDown() {
+		input.keyPress(KeyEvent.VK_S);
+		input.keyRelease(KeyEvent.VK_S);
+	}
+	
+	static void pressUp() {
+		input.keyPress(38);
+		input.keyRelease(38);
+	}
+	
+	static void pressRight() {
+		input.keyPress(KeyEvent.VK_D);
+		input.keyRelease(KeyEvent.VK_D);
+	}
+	
+	static void pressLeft() {
+		input.keyPress(KeyEvent.VK_A);
+		input.keyRelease(KeyEvent.VK_A);
+	}
+	
+	static void pressSpace() {
+		input.keyPress(KeyEvent.VK_SPACE);
+		input.keyRelease(KeyEvent.VK_SPACE);
 	}
 }
